ba683868d3ea26191ab1378febfdb5bb9315ca7f
Vincent <Vincent.ahlin99@gmail.com>
2023-09-23 21:09:08 +0200

    Removed var colors

--- PortfolioV2/app.js
+++ PortfolioV2/app.js
@@ -1,27 +1,3 @@
-// let innerCursor = document.querySelector(".innerCursor");
-// let outerCursor = document.querySelector(".outerCursor");
-
-// document.addEventListener("mousemove", moveCursor);
-
-// function moveCursor(e) {
-//   let x = e.clientX;
-//   let y = e.clientY;
-//   innerCursor.style.left = `${x}px`;
-//   innerCursor.style.top = `${y}px`;
-
-//   setTimeout(() => {
-//     outerCursor.style.left = `${x}px`;
-//     outerCursor.style.top = `${y}px`;
-//   }, 190);
-// }
-
-// const navToggle = document.querySelector(".mobileNavToggle");
-// const primaryNav = document.querySelector(".navItems");
-
-// navToggle.add.addEventListener("click", () => {
-//   primaryNav.classList.toggle("opened");
-// });
-
 const menu = document.querySelector(".navItems");
 const menuItems = document.querySelectorAll(".navItems");
 const hamburger = document.querySelector(".mobileNavToggle");
@@ -40,10 +16,6 @@ function toggleMenu() {
   }
 }
 hamburger.addEventListener("click", toggleMenu);
-
-const darkMode = document.querySelector(".darkModeSwitch");
-const lightMode = document.querySelector(".lightModeSwitch");
-const theme = document.querySelector(".theme-switch");
 
 const innerCursor = document.querySelector(".innerCursor");
 const outerCursor = document.querySelector(".outerCursor");
@@ -92,29 +64,3 @@ links.forEach((link) => {
     innerCursor.classList.add("fade");
   });
 });
-
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
-
-const currentTheme = localStorage.getItem("theme");
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
-}
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
